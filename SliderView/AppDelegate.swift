47b9f6f090822a35ead76fdc5b32e218952702b5
ccl <2711056454@qq.com>
2018-11-16 00:36:37 +0800

    修改appdelete

--- SliderView/AppDelegate.swift
+++ SliderView/AppDelegate.swift
@@ -15,13 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window!.backgroundColor = UIColor.white
-        let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
-        let controller = storyboard.instantiateInitialViewController()
-        window!.rootViewController = controller
-       
         return true
     }
     
